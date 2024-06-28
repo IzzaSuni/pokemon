9ff804448d143e80ece7dd2b9a3fce247edf426a
Akbar Izza Mahendra Suni <akbarizza09@gmail.com>
2024-06-28 22:29:45 +0700

    package

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,7 +1,3 @@
-import useGetListPokemons from "@/src/network/useGetListPokemons";
-
 export default function ListPokemons() {
-  const { data } = useGetListPokemons({ page: 1 });
-
   return <>hi</>;
 }
