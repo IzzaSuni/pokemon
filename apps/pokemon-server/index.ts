54c1771aae5e237eab2efda745973095fb77270c
Akbar Izza Mahendra Suni <akbarizza09@gmail.com>
2024-07-05 04:35:36 +0700

    vercel json

--- apps/pokemon-server/index.ts
+++ apps/pokemon-server/index.ts
@@ -2,6 +2,10 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+app.get("/api", (req, res) => {
+  return res.send("ahoy");
+});
+
 app.listen(3001, () => {
   console.log("server run");
 });
