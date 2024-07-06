3a822ca0038879632e34e21e5b1ceefd9e54e1bf
Akbar Izza Mahendra Suni <akbarizza09@gmail.com>
2024-07-06 21:08:41 +0700

    build issue

--- apps/pokemon-server/index.ts
+++ apps/pokemon-server/index.ts
@@ -8,7 +8,6 @@ import { rateLimit } from "express-rate-limit";
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
   limit: 100,
-  standardHeaders: "draft-7",
   legacyHeaders: false,
 });
 
